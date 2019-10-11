069dd9634f9883cdea84345f240db7f00400045e
Callum Hayden <haydencallum4@gmail.com>
2019-10-11 18:43:52 +0100

    Start on coursework 1

---
+++ Coursework1/Webbrowser_console/Webbrowser_console/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace Webbrowser_console
+{
+    class Program
+    {
+        public static string getHtml(string url)
+        {
+            string html = string.Empty;
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                   | SecurityProtocolType.Tls11
+                   | SecurityProtocolType.Tls12
+                   | SecurityProtocolType.Ssl3;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                {
+                    html = reader.ReadToEnd();
+                }
+
+                return html;
+            } catch (WebException e) // Works with normal exception too.
+            {
+                return e.Message;
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Please insert URL");
+            string url = Console.ReadLine();
+            //string url = "http://www.contoso.com/";
+            string html = getHtml(url);
+
+            Console.WriteLine(html);
+
+
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        
+    }
+}
