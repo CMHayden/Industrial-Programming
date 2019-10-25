189802ca6d74c66697d0e48bcb5bd501241ba2c1
Callum Hayden <haydencallum4@gmail.com>
2019-10-25 21:27:50 +0100

    Add files via upload

---
+++ Coursework1/webBrowserGUIApplication/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace webBrowserGUIApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.textBoxHtml = new System.Windows.Forms.TextBox();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.buttonFavourites = new System.Windows.Forms.Button();
+            this.buttonHistory = new System.Windows.Forms.Button();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.listBoxHisFav = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.goHomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setHomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBoxFavs = new System.Windows.Forms.ListBox();
+            this.labelRename = new System.Windows.Forms.Label();
+            this.textBoxRename = new System.Windows.Forms.TextBox();
+            this.buttonRename = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(156, 11);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(808, 20);
+            this.textBoxURL.TabIndex = 0;
+            // 
+            // textBoxHtml
+            // 
+            this.textBoxHtml.AccessibleName = "html";
+            this.textBoxHtml.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBoxHtml.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBoxHtml.Location = new System.Drawing.Point(12, 38);
+            this.textBoxHtml.Multiline = true;
+            this.textBoxHtml.Name = "textBoxHtml";
+            this.textBoxHtml.ReadOnly = true;
+            this.textBoxHtml.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxHtml.Size = new System.Drawing.Size(1048, 680);
+            this.textBoxHtml.TabIndex = 1;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.AccessibleName = "Back";
+            this.buttonBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonBack.Enabled = false;
+            this.buttonBack.Location = new System.Drawing.Point(12, 9);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(42, 23);
+            this.buttonBack.TabIndex = 2;
+            this.buttonBack.Text = "←";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.AccessibleDescription = "";
+            this.buttonForward.AccessibleName = "forward";
+            this.buttonForward.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonForward.Enabled = false;
+            this.buttonForward.Location = new System.Drawing.Point(60, 9);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Size = new System.Drawing.Size(42, 23);
+            this.buttonForward.TabIndex = 3;
+            this.buttonForward.Text = "→\t";
+            this.buttonForward.UseVisualStyleBackColor = true;
+            this.buttonForward.Click += new System.EventHandler(this.buttonForward_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.AccessibleName = "refresh";
+            this.buttonRefresh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonRefresh.Location = new System.Drawing.Point(108, 9);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(42, 23);
+            this.buttonRefresh.TabIndex = 4;
+            this.buttonRefresh.Text = "↻";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.AccessibleName = "go";
+            this.buttonGo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonGo.Location = new System.Drawing.Point(970, 9);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(42, 23);
+            this.buttonGo.TabIndex = 5;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // buttonFavourites
+            // 
+            this.buttonFavourites.AccessibleName = "Favourites";
+            this.buttonFavourites.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonFavourites.Location = new System.Drawing.Point(1114, 9);
+            this.buttonFavourites.Name = "buttonFavourites";
+            this.buttonFavourites.Size = new System.Drawing.Size(42, 23);
+            this.buttonFavourites.TabIndex = 7;
+            this.buttonFavourites.Text = "★";
+            this.buttonFavourites.UseVisualStyleBackColor = true;
+            this.buttonFavourites.Click += new System.EventHandler(this.buttonFavourites_Click);
+            // 
+            // buttonHistory
+            // 
+            this.buttonHistory.AccessibleName = "History";
+            this.buttonHistory.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonHistory.Location = new System.Drawing.Point(1162, 9);
+            this.buttonHistory.Name = "buttonHistory";
+            this.buttonHistory.Size = new System.Drawing.Size(42, 23);
+            this.buttonHistory.TabIndex = 8;
+            this.buttonHistory.Text = "⏰";
+            this.buttonHistory.UseVisualStyleBackColor = true;
+            this.buttonHistory.Click += new System.EventHandler(this.buttonHistory_Click);
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.AccessibleName = "home";
+            this.buttonHome.AllowDrop = true;
+            this.buttonHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonHome.Location = new System.Drawing.Point(1018, 9);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(42, 23);
+            this.buttonHome.TabIndex = 10;
+            this.buttonHome.Text = "🏠";
+            this.buttonHome.UseVisualStyleBackColor = true;
+            this.buttonHome.Click += new System.EventHandler(this.buttonHome_Click);
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.AccessibleDescription = "status";
+            this.textBoxStatus.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBoxStatus.Location = new System.Drawing.Point(12, 724);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.ReadOnly = true;
+            this.textBoxStatus.Size = new System.Drawing.Size(1048, 20);
+            this.textBoxStatus.TabIndex = 11;
+            // 
+            // listBoxHisFav
+            // 
+            this.listBoxHisFav.AccessibleDescription = "history and favs";
+            this.listBoxHisFav.FormattingEnabled = true;
+            this.listBoxHisFav.Location = new System.Drawing.Point(1114, 38);
+            this.listBoxHisFav.Name = "listBoxHisFav";
+            this.listBoxHisFav.Size = new System.Drawing.Size(263, 706);
+            this.listBoxHisFav.TabIndex = 12;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.goHomeToolStripMenuItem,
+            this.setHomeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(68, 48);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
+            // goHomeToolStripMenuItem
+            // 
+            this.goHomeToolStripMenuItem.Name = "goHomeToolStripMenuItem";
+            this.goHomeToolStripMenuItem.Size = new System.Drawing.Size(67, 22);
+            // 
+            // setHomeToolStripMenuItem
+            // 
+            this.setHomeToolStripMenuItem.Name = "setHomeToolStripMenuItem";
+            this.setHomeToolStripMenuItem.Size = new System.Drawing.Size(67, 22);
+            // 
+            // listBoxFavs
+            // 
+            this.listBoxFavs.AccessibleDescription = "history and favs";
+            this.listBoxFavs.FormattingEnabled = true;
+            this.listBoxFavs.Location = new System.Drawing.Point(1114, 39);
+            this.listBoxFavs.Name = "listBoxFavs";
+            this.listBoxFavs.Size = new System.Drawing.Size(263, 706);
+            this.listBoxFavs.TabIndex = 13;
+            // 
+            // labelRename
+            // 
+            this.labelRename.AutoSize = true;
+            this.labelRename.Enabled = false;
+            this.labelRename.Location = new System.Drawing.Point(1159, 138);
+            this.labelRename.Name = "labelRename";
+            this.labelRename.Size = new System.Drawing.Size(172, 13);
+            this.labelRename.TabIndex = 14;
+            this.labelRename.Text = "Edit the name in the text box below";
+            this.labelRename.Visible = false;
+            // 
+            // textBoxRename
+            // 
+            this.textBoxRename.Enabled = false;
+            this.textBoxRename.Location = new System.Drawing.Point(1162, 154);
+            this.textBoxRename.Name = "textBoxRename";
+            this.textBoxRename.Size = new System.Drawing.Size(169, 20);
+            this.textBoxRename.TabIndex = 15;
+            this.textBoxRename.Visible = false;
+            // 
+            // buttonRename
+            // 
+            this.buttonRename.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonRename.Enabled = false;
+            this.buttonRename.Location = new System.Drawing.Point(1209, 180);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(75, 23);
+            this.buttonRename.TabIndex = 16;
+            this.buttonRename.Text = "Rename";
+            this.buttonRename.UseVisualStyleBackColor = true;
+            this.buttonRename.Visible = false;
+            this.buttonRename.Click += new System.EventHandler(this.buttonRename_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1389, 757);
+            this.Controls.Add(this.buttonRename);
+            this.Controls.Add(this.textBoxRename);
+            this.Controls.Add(this.labelRename);
+            this.Controls.Add(this.listBoxFavs);
+            this.Controls.Add(this.listBoxHisFav);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.buttonHome);
+            this.Controls.Add(this.buttonHistory);
+            this.Controls.Add(this.buttonFavourites);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonForward);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.textBoxHtml);
+            this.Controls.Add(this.textBoxURL);
+            this.Name = "Form1";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.TextBox textBoxHtml;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.Button buttonFavourites;
+        private System.Windows.Forms.Button buttonHistory;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.ListBox listBoxHisFav;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem goHomeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setHomeToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBoxFavs;
+        private System.Windows.Forms.Label labelRename;
+        private System.Windows.Forms.TextBox textBoxRename;
+        private System.Windows.Forms.Button buttonRename;
+    }
+}
+
---
+++ Coursework1/webBrowserGUIApplication/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.IO;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace webBrowserGUIApplication
+{
+    public partial class Form1 : Form
+    {
+
+        public static Stack back = new Stack();
+        public static Stack forward = new Stack();
+        public static string home = "http://www.callumhayden.com";
+        public static string url = home;
+        public static string title;
+        public static IDictionary<string, string> history = new Dictionary<string,string>();
+        public static Dictionary<string, string> favourites = new Dictionary<string, string>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getPage(home);
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            back.Push(url);
+            buttonBack.Enabled = true;
+            url = textBoxURL.Text;
+            getPage(url);
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            string temp = back.Pop().ToString();
+            if (back.Count == 0)
+                buttonBack.Enabled = false;
+            forward.Push(temp);
+            buttonForward.Enabled = true;
+            displayHTML(temp);
+        }
+
+        private void buttonForward_Click(object sender, EventArgs e)
+        {
+            string temp = forward.Pop().ToString();
+            back.Push(temp);
+            if (forward.Count == 0)
+                buttonForward.Enabled = false;
+            buttonBack.Enabled = true;
+            displayHTML(temp);
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            textBoxHtml.Text = string.Empty;
+            getPage(url);
+        }
+
+        private void buttonHistory_Click(object sender, EventArgs e)
+        {
+            listBoxFavs.Visible = false;
+            listBoxHisFav.Visible = true;
+            displayHistory();
+        }
+
+        private void listBoxHisFav_DoubleClick(object sender, EventArgs e)
+        {
+            displayHTML(listBoxHisFav.GetItemText(listBoxHisFav.SelectedValue));
+            textBoxURL.Text = listBoxHisFav.GetItemText(listBoxHisFav.SelectedValue);
+        }
+
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Items.Clear();
+            contextMenuStrip1.Items.Add("Go home");
+            contextMenuStrip1.Items.Add("Set home");
+            contextMenuStrip1.Show(buttonHome, new Point(0, buttonHome.Height));
+        }
+
+        private void buttonFavourites_Click(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Items.Clear();
+            if (!favourites.ContainsValue(url))
+            {
+                contextMenuStrip1.Items.Add("Add to favourites");
+            } else
+            {
+                contextMenuStrip1.Items.Add("Remove from favourites");
+            }
+            if(!(favourites.Count == 0))
+                contextMenuStrip1.Items.Add("View favourites");
+            contextMenuStrip1.Show(buttonFavourites, new Point(0, buttonHome.Height));
+        }
+
+        private void listBoxFavs_Click(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Items.Clear();
+            contextMenuStrip1.Items.Add("Go to url");
+            contextMenuStrip1.Items.Add("Rename favourite");
+            contextMenuStrip1.Items.Add("Remove from favourites");
+            contextMenuStrip1.Show(listBoxFavs, new Point(0, listBoxFavs.Top));
+        }
+
+        private void buttonRename_Click(object sender, EventArgs e)
+        {
+            string newName = textBoxRename.Text;
+            string oldUrl = listBoxFavs.GetItemText(listBoxFavs.SelectedValue);
+            removeFav(listBoxFavs.GetItemText(listBoxFavs.SelectedValue));
+            favourites.Add(newName, oldUrl);
+
+            buttonRename.Enabled = false;
+            buttonRename.Visible = false;
+            textBoxRename.Enabled = false;
+            textBoxRename.Visible = false;
+            labelRename.Enabled = false;
+            labelRename.Visible = false;
+
+
+
+        }
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Text)
+            {
+                case "Go home":
+                    textBoxURL.Text = home;
+                    displayHTML(home);
+                    break;
+
+                case "Set home":
+                    home = url;
+                    break;
+
+                case "Add to favourites":
+                    favourites.Add(title, url);
+                    break;
+
+                case "View favourites":
+                    displayFavs();
+                    break;
+
+                case "Remove from favourites":
+
+                    removeFav(listBoxFavs.GetItemText(listBoxFavs.SelectedValue));
+                    break;
+
+                case "Go to url":
+                    displayHTML(listBoxFavs.GetItemText(listBoxFavs.SelectedValue));
+                    textBoxURL.Text = listBoxFavs.GetItemText(listBoxFavs.SelectedValue);
+                    break;
+
+                case "Rename favourite":
+                    buttonRename.Enabled = true;
+                    buttonRename.Visible = true;
+                    textBoxRename.Enabled = true;
+                    textBoxRename.Visible = true;
+                    labelRename.Enabled = true;
+                    labelRename.Visible = true;
+                    break;
+            }
+        }
+
+
+
+
+
+
+
+        /*
+         * Shit that does mad tingz blud
+         */
+
+        public void removeFav(string val)
+        {
+            var remove = favourites.First(kvp => kvp.Value == val);
+
+            favourites.Remove(remove.Key);
+            listBoxFavs.Visible = false;
+            
+        }
+
+        public void getPage(string url)
+        {
+            try
+            {
+                new Uri(url);
+                displayHTML(url);
+            }
+            catch
+            {
+                textBoxHtml.Text = "Error: invalid URL. Please ensure you use 'http://' at the start of your url";
+            }
+
+        }
+
+        public void displayFavs()
+        {
+            listBoxHisFav.Visible = false;
+            listBoxFavs.Visible = true;
+            listBoxFavs.Refresh();
+            listBoxFavs.DataSource = new BindingSource(favourites, null);
+            listBoxFavs.DisplayMember = "key";
+            listBoxFavs.ValueMember = "value";
+            listBoxFavs.Click += new EventHandler(listBoxFavs_Click);
+        }
+
+        public void displayHistory()
+        {
+            listBoxHisFav.Refresh();
+            listBoxHisFav.DataSource = new BindingSource(history, null);
+            listBoxHisFav.DisplayMember = "key";
+            listBoxHisFav.ValueMember = "value";
+            listBoxHisFav.DoubleClick += new EventHandler(listBoxHisFav_DoubleClick);
+        }
+
+        public void displayHTML(string url)
+        {
+            string html = string.Empty;
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                   | SecurityProtocolType.Tls11
+                   | SecurityProtocolType.Tls12
+                   | SecurityProtocolType.Ssl3;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Console.WriteLine(response.StatusCode);
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                {
+                    html = reader.ReadToEnd();
+                }
+
+                title = Regex.Match(html, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>",
+                    RegexOptions.IgnoreCase).Groups["Title"].Value;
+
+                Text = title;
+                textBoxStatus.Text = response.StatusCode.ToString();
+                textBoxHtml.Text = html.ToString();
+                if (!history.ContainsKey(title))
+                    history.Add(title, url);
+
+            }
+            catch (WebException e)
+            {
+                textBoxHtml.Text = String.Empty;
+                textBoxStatus.Text = e.Message;
+            }
+        }
+    }
+    
+}
